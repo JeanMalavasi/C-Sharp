c24c23704d4988904a474c1b9e93dd6d260a0628
JEAN PINTO MALAVASI <73088164+JeanMalavasi@users.noreply.github.com>
2020-10-19 19:56:16 -0300

    Add files via upload

---
+++ Sintaxe/ControleDeFluxo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ControleDeFluxo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IF
+            int idade = 15;
+            bool acompanhado = true;
+            if (idade > 18 || acompanhado)
+                Console.WriteLine("Permitido.");
+            else{
+                Console.WriteLine("Negado.");
+            }
+
+            //WHILE
+            double investimento = 1000;
+            int meses = 1;           
+            while (meses <= 12)
+            {
+                investimento += investimento * 0.0036;
+                Console.WriteLine($"Investimento: {investimento}");
+                meses++;
+            }
+
+            //FOR
+            investimento = 1000;
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                investimento += investimento * 0.0036;
+                Console.WriteLine($"Investimento: {investimento}");
+            }
+        }
+    }
+}
---
+++ Sintaxe/Variaveis/Variaveis.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Variaveis
+{
+    class Variaveis
+    {
+        static void Main(string[] args)
+        {
+            //INTEIRO
+            int idade = 32;
+            Console.WriteLine($"Idade: {idade}");
+            //idade = idade + 10
+            idade += 10;
+            Console.WriteLine($"Idade: {idade}");
+
+            //FLOAT
+            double salario = 2000.50;
+            Console.WriteLine($"Salario: {salario}");
+            //Divisão para retornar com ponto flutuante
+            salario = 15.0 / 2;
+            Console.WriteLine($"Salario: {salario}");
+
+            //CARACTERE E STRING
+            char letra = 'a';
+            Console.WriteLine($"Letra: {letra}");
+
+            string texto = "Hello word!";
+            Console.WriteLine($"Texto: {texto}");
+        }
+    }
+}
